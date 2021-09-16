b0fe52b02fe41480132d6f1e276da46e098a7667
frankie <fviramontes8@unm.edu>
2021-09-16 08:12:38 -0600

    Add program example that prints the addesses and contents of pointer

--- Pointers/printpointer.c
+++ Pointers/printpointer.c
@@ -5,11 +5,18 @@ int main() {
 	int z = 1;
 
 	int *y;
-	y = &x;
+	y = &x; // & is address operator
+
+	int *q = &x; // Does lines 7 and 8 in one line
 
 	printf("%p, %p\n", &x, &z);
+	printf("%p, %d, %p\n", y, *y, &y);
 
-	printf("%p, %d\n", y, *y);
+	float a = 42.0;
+	float *b = &a;
+
+	float c = (*y) * (*b);
+	printf("%f\n", c);
 
 	return 0;
 }
