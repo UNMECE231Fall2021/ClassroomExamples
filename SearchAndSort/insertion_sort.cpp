be12dc79f58ee3af4c7469500082fdd3c7357b09
frankie <fviramontes8@unm.edu>
2021-11-15 19:31:34 -0700

    Update insertion_sort
    Updated insertion sort to follow notes and visualization example

--- SearchAndSort/insertion_sort.cpp
+++ SearchAndSort/insertion_sort.cpp
@@ -8,7 +8,22 @@ void swap(T &x, T &y) {
 	y = temp;
 }
 
+// n ( 1 + 2 + 3 + ... + n-1) = (n * (n-1)) / 2 ~ n^2
+
+template <typename T>
+void insertion_sort(std::vector<T> &v) {
+	int size = v.size();
+	for (int i=1; i<size; ++i) {
+		for (int j=0; j<i+1; ++j) {
+			if (v[i] < v[j]) {
+				swap(v[i], v[j]);
+			}
+		}
+	}
+}
+
 int main() {
+	std::vector<int> x = {56, 2, 14, 9, 7};
 
 	return 0;
 }
